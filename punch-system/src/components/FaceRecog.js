89e2fdb549aea3809f8e84da61ffc7f01916ef19
VishnuVardhanBonala-web4U <rvardhancm08@gmail.com>
2025-02-16 17:28:21 +0530

    .

--- punch-system/src/components/FaceRecog.js
+++ punch-system/src/components/FaceRecog.js
@@ -3,6 +3,7 @@ import Webcam from "react-webcam";
 import * as faceapi from "@vladmandic/face-api";
 import { toast } from "react-toastify";
 import axios from "axios";
+import base_url from './../const';
 
 const FacialRecognition = ({ onPunchUpdate }) => {
   const webcamRef = useRef(null);
@@ -90,7 +91,7 @@ const FacialRecognition = ({ onPunchUpdate }) => {
 
         // Send data to backend
         try {
-          await axios.post("http://localhost:5000/api/locations", punchDetails);
+          await axios.post( `${base_url}api/locations`, punchDetails);
           toast.success(`Punch recorded successfully at ${timestamp}.`);
         } catch (error) {
           toast.error("Failed to sync punch data.");
---
+++ punch-system/src/const.js
@@ -0,0 +1,3 @@
+export  const  base_url = "https://test.talentspotify.com/";
+// export default base_url = " "
+
