25c04ad60a10ca0a3b8cebdc1dd26146ed1f965a
VishnuVardhanBonala-web4U <rvardhancm08@gmail.com>
2025-02-16 23:29:33 +0530

    .

--- puch-system-node/consts/Consts.js
+++ puch-system-node/consts/Consts.js
@@ -1,5 +1,4 @@
-// export const MONGO_URI =
-//   "mongodb+srv://prashanth:BnHRQrqZHdnosfEe@cluster0.cpydc.mongodb.net/location-db?retryWrites=true&w=majority";
+export const MONGO_URI =  "mongodb+srv://prashanth:BnHRQrqZHdnosfEe@cluster0.cpydc.mongodb.net/location-db?retryWrites=true&w=majority";
 
-export const MONGO_URI =
-  "mongodb://localhost:27017/location-db"
+// export const MONGO_URI =
+//   "mongodb://localhost:27017/location-db
